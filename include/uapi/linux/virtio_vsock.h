6660977326b8760d2ea4bdbce271db351c060cd2
Bobby Eshleman <bobby.eshleman@bytedance.com>
2023-06-10 00:58:32 +0000

    0007-virtio-vsock-add-VIRTIO_VSOCK_F_DGRAM-feature-bit: virtio/vsock: add VIRTIO_VSOCK_F_DGRAM feature bit

--- include/uapi/linux/virtio_vsock.h
+++ include/uapi/linux/virtio_vsock.h
@@ -40,6 +40,7 @@
 
 /* The feature bitmap for virtio vsock */
 #define VIRTIO_VSOCK_F_SEQPACKET	1	/* SOCK_SEQPACKET supported */
+#define VIRTIO_VSOCK_F_DGRAM		3	/* SOCK_DGRAM supported */
 
 struct virtio_vsock_config {
 	__le64 guest_cid;
